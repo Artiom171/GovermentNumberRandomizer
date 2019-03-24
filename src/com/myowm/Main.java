8262ac10e69a839609343502b39896140fc64566
Artiom171 <artiom201097@gmail.com>
2019-03-24 22:13:44 +0200

    Vehicle class created, change the business logic of the project

--- src/com/myowm/Main.java
+++ src/com/myowm/Main.java
@@ -2,10 +2,11 @@ package com.myowm;
 
 
 import java.util.Random;
-
+import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
+        ArrayList<Vehicle> vehicles = new ArrayList<Vehicle>();
         char letter = 'a';
         char[] letters = new char[26];
         for (int i = 0; i < 26; i++) {
@@ -20,7 +21,7 @@ public class Main {
             System.out.print(random.nextInt(999));
             System.out.println();
         }
-
+        
 
     }
 }
---
+++ src/com/myowm/Vehicle.java
@@ -0,0 +1,67 @@
+package com.myowm;
+
+public class Vehicle {
+    private int id;
+    private String licensePlateNumber;
+    private String model;
+    private String brand;
+    private String carcass;
+    private String petrol;
+    private String power;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLicensePlateNumber() {
+        return licensePlateNumber;
+    }
+
+    public void setLicensePlateNumber(String licensePlateNumber) {
+        this.licensePlateNumber = licensePlateNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getCarcass() {
+        return carcass;
+    }
+
+    public void setCarcass(String carcass) {
+        this.carcass = carcass;
+    }
+
+    public String getPetrol() {
+        return petrol;
+    }
+
+    public void setPetrol(String petrol) {
+        this.petrol = petrol;
+    }
+
+    public String getPower() {
+        return power;
+    }
+
+    public void setPower(String power) {
+        this.power = power;
+    }
+}
